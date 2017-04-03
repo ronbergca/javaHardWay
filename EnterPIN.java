61fedf304c136b7d398bb7f720ecddea89961179
Chris Ronberg <ronbergca@gmail.com>
2017-04-02 22:03:02 -0600

    ex: 25

---
+++ EnterPIN.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class EnterPIN {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		int pin, entry;
+		String password = "foxhound", enter;
+		
+		pin = 12345;
+				
+		System.out.println( "WELCOME TO THE BANK OF JAVA. ");
+		System.out.println( "ENTER YOUR PASSWORD: ");
+		enter = keyboard.next();
+		
+		while(! enter.equals(password) ) {
+			System.out.println( "\nINCORRECT PASSWORD. TRY AGAIN.");
+			System.out.println( "ENTER YOUR PASSWORD: ");
+			enter = keyboard.next();
+		}
+		
+		System.out.println( "ENTER YOUR PIN: ");
+		entry = keyboard.nextInt();
+		
+		while( entry != pin ) {
+			System.out.println( "\nINCORRECT PIN. TRY AGAIN.");
+			System.out.println( "ENTER YOUR PIN: ");
+			entry = keyboard.nextInt();
+		}
+		
+		System.out.println("\nPIN ACCEPTED. YOUR ACCOUNT BALANCE IS $425.17");
+	}
+}
