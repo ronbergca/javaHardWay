dd768fc1731cbee67b7f4521b0b31384f07a1315
Chris Ronberg <ronbergca@gmail.com>
2017-04-02 21:51:45 -0600

    ex: 3

---
+++ PrintChoices.java
@@ -0,0 +1,29 @@
+/*
+ * @Author: Chris Ronberg
+ * @Date Created: 3/28/2017
+ * @Description: Exercise 3- Demonstrates difference in println and print.
+ * 
+ */
+ 
+ public class PrintChoices {
+ 	 public static void main(String[]args){
+ 	 	 System.out.println("Alpha");
+ 	 	 System.out.println("Bravo");
+ 	 	 
+ 	 	 System.out.println("Charlie");
+ 	 	 System.out.println("Delta");
+ 	 	 System.out.println();
+ 	 	 
+ 	 	 System.out.print("Echo");
+ 	 	 System.out.print("Foxtrot");
+ 	 	 
+ 	 	 System.out.println("Golf");
+ 	 	 System.out.print("Hotel");
+ 	 	 System.out.println();
+ 	 	 System.out.println("India");
+ 	 	 
+ 	 	 System.out.println();
+ 	 	 System.out.println("This" + " " + "is" + " " + "a" + " " + "test.");
+ 	 	 System.out.println("I am" + " " + "learning Java" + " " + "the Hard Way!");
+ 	 }
+ }
