0a190fe65cf7d3129c52dc2520a13327b6e29a05
Chris Ronberg <ronbergca@gmail.com>
2017-04-02 21:55:53 -0600

    ex: 10

---
+++ Sequencing.java
@@ -0,0 +1,26 @@
+/*
+ * @Author: Chris Ronberg
+ * @Date Created: 3/28/2017
+ * @Description: Exercise 10- Variables only hold values
+ * 
+ */
+ 
+ import java.util.Scanner;
+ 
+ public class Sequencing {
+ 	 public static void main( String[] args ){
+ 	 	 
+ 	 	 Scanner keyboard = new Scanner(System.in);
+ 	 	 double price, salesTax, total; 	 
+
+ 	 	 System.out.print( "How much is the purchase price?" );
+ 	 	 price = keyboard.nextDouble();
+ 	 	 
+ 	 	 salesTax = price * 0.0825;
+ 	 	 total = price + salesTax;
+ 	 	 
+ 	 	 System.out.println( "Item price:\t" + price );
+ 	 	 System.out.println( "Sales tax:\t" + salesTax );
+ 	 	 System.out.println( "Total cost:\t" + total );
+ 	 }
+ }
