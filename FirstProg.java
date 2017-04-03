653304751b31c4e7a1914b1d767e95d6f7c1a075
Chris Ronberg <ronbergca@gmail.com>
2017-04-02 21:39:24 -0600

    exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,14 @@
+/*
+ * @Author: Chris Ronberg
+ * @Date Created: 3/28/2017
+ * @Description: Exercise 1- Display an important message on the screen.
+ * 
+ */
+ 
+ public class FirstProg {
+ 	 public static void main(String[]args){
+ 	 	 //use to display text
+ 	 	 System.out.println( "I, Chris Ronberg, am determined to learn how to code.");
+ 	 	 System.out.println( "Today's date is: 3/28/2017");
+ 	 }
+ }
