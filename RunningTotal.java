4153deae6239b04480dc872cc610a59581ff78e2
Chris Ronberg <ronbergca@gmail.com>
2017-04-02 22:04:46 -0600

    ex: 30

---
+++ RunningTotal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RunningTotal {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		int current, total = 0;
+		
+		System.out.println( "Type in a bunch of values and I'll add them up. ");
+		System.out.println( "I'll stop when you type a zero.");
+		System.out.print( "Value: ");
+		current = keyboard.nextInt();
+		
+		do {
+			//while ( current != 0) {
+				total += current;
+				System.out.println( "The total so far is: " +total);
+				System.out.print( "Value: ");
+				current = keyboard.nextInt();
+		} while ( current != 0 );
+		
+		System.out.println( "The final total is: " +total);
+	}
+}
